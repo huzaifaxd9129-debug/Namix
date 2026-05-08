c4af2bf16ca8412a59704959cf3db1b59546538f
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 22:07:12 +0500

    guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,35 @@
+const {
+EmbedBuilder
+} = require("discord.js");
 
+module.exports = async (client, member) => {
+
+const channel = member.guild.channels.cache.find(
+c => c.name === "welcome"
+);
+
+if (!channel) return;
+
+const embed = new EmbedBuilder()
+.setColor("Blue")
+.setTitle("👋 Welcome")
+.setDescription(`
+Welcome ${member}
+
+You joined **${member.guild.name}**
+You are member #${member.guild.memberCount}
+`)
+.setThumbnail(
+member.user.displayAvatarURL({ dynamic: true })
+)
+.setFooter({
+text: `ID: ${member.user.id}`
+})
+.setTimestamp();
+
+channel.send({
+content: `${member}`,
+embeds: [embed]
+});
+
+};
