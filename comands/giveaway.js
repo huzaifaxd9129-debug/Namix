11fee7352228c53c7c808dc2f7175f50e9c3a497
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 22:03:47 +0500

    giveaway.js

--- comands/giveaway.js
+++ comands/giveaway.js
@@ -1,1 +1,28 @@
+module.exports = {
+name: "giveaway",
 
+async run(client, message, args, cmd) {
+
+if (cmd === "gstart") {
+
+message.channel.send(
+`Giveaway started for ${args.join(" ")}`
+);
+}
+
+if (cmd === "gend") {
+
+message.channel.send(
+"Giveaway ended"
+);
+}
+
+if (cmd === "greroll") {
+
+message.channel.send(
+"Giveaway rerolled"
+);
+}
+
+}
+};
