a1f72aaf595c862ab897cbbc6c418148fd251ad2
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 21:59:28 +0500

    automod.js

--- systems/automod.js
+++ systems/automod.js
@@ -1,1 +1,23 @@
+module.exports = async (message) => {
 
+const blocked = ["badword1", "badword2"];
+
+if (blocked.some(w => message.content.includes(w))) {
+
+await message.delete();
+
+message.channel.send(
+`${message.author}, bad words not allowed`
+);
+}
+
+if (message.content.includes("http")) {
+
+await message.delete();
+
+message.channel.send(
+`${message.author}, links blocked`
+);
+}
+
+};
