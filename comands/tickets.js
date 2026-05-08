7cbad5911f84027c09315d88c61064f9b5ae60fb
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 22:02:33 +0500

    tickets.js

--- comands/tickets.js
+++ comands/tickets.js
@@ -1,1 +1,37 @@
+const {
+EmbedBuilder,
+ActionRowBuilder,
+ButtonBuilder,
+ButtonStyle
+} = require("discord.js");
 
+module.exports = {
+name: "tickets",
+
+async run(client, message, args, cmd) {
+
+if (cmd === "ticketpanel") {
+
+const embed = new EmbedBuilder()
+.setColor("Blue")
+.setTitle("Support System")
+.setDescription(
+"Click button below to open ticket"
+);
+
+const row = new ActionRowBuilder()
+.addComponents(
+new ButtonBuilder()
+.setCustomId("create_ticket")
+.setLabel("Open Ticket")
+.setStyle(ButtonStyle.Primary)
+);
+
+message.channel.send({
+embeds: [embed],
+components: [row]
+});
+}
+
+}
+};
