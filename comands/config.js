6df23c80742f48c288d001e6803ffbc67d644ec3
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 22:05:01 +0500

    config.js

--- comands/config.js
+++ comands/config.js
@@ -1,1 +1,28 @@
+module.exports = {
+name: "config",
 
+async run(client, message, args, cmd) {
+
+if (cmd === "setlogs") {
+
+message.channel.send(
+"Logs channel configured"
+);
+}
+
+if (cmd === "setwelcome") {
+
+message.channel.send(
+"Welcome channel configured"
+);
+}
+
+if (cmd === "settickets") {
+
+message.channel.send(
+"Ticket category configured"
+);
+}
+
+}
+};
