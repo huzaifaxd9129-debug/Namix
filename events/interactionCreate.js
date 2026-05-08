357cf64e6fe9c671b64d654368da77cb8a8cb7e0
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 22:03:03 +0500

    interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,76 @@
+const {
+ChannelType,
+PermissionsBitField,
+ActionRowBuilder,
+ButtonBuilder,
+ButtonStyle
+} = require("discord.js");
 
+module.exports = async (client, interaction) => {
+
+if (!interaction.isButton()) return;
+
+if (interaction.customId === "create_ticket") {
+
+const channel =
+await interaction.guild.channels.create({
+
+name: `ticket-${interaction.user.username}`,
+
+type: ChannelType.GuildText,
+
+permissionOverwrites: [
+{
+id: interaction.guild.id,
+deny: [
+PermissionsBitField.Flags.ViewChannel
+]
+},
+{
+id: interaction.user.id,
+allow: [
+PermissionsBitField.Flags.ViewChannel
+]
+}
+]
+});
+
+const row = new ActionRowBuilder()
+.addComponents(
+
+new ButtonBuilder()
+.setCustomId("claim_ticket")
+.setLabel("Claim")
+.setStyle(ButtonStyle.Primary),
+
+new ButtonBuilder()
+.setCustomId("close_ticket")
+.setLabel("Close")
+.setStyle(ButtonStyle.Danger)
+);
+
+channel.send({
+content: `${interaction.user}`,
+components: [row]
+});
+
+interaction.reply({
+content: `Ticket created: ${channel}`,
+ephemeral: true
+});
+}
+
+if (interaction.customId === "close_ticket") {
+
+await interaction.channel.delete();
+}
+
+if (interaction.customId === "claim_ticket") {
+
+interaction.reply({
+content: `Ticket claimed by ${interaction.user}`,
+ephemeral: false
+});
+}
+
+};
