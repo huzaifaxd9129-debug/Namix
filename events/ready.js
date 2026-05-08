91b97425685e8a6bf30b1165ef6ea652dce863f1
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 22:10:49 +0500

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,28 @@
+const {
+joinVoiceChannel
+} = require("@discordjs/voice");
+
 module.exports = async (client) => {
 
 console.log(`${client.user.tag} online`);
 
 client.user.setActivity("⚡ Moderating Premuim Servers");
+
+const guild = client.guilds.cache.get("1428738026674192527");
+
+if (!guild) return;
+
+const channel = guild.channels.cache.get("1500169321991766277");
+
+if (!channel) return;
+
+joinVoiceChannel({
+channelId: channel.id,
+guildId: guild.id,
+adapterCreator: guild.voiceAdapterCreator,
+selfDeaf: false
+});
+
+console.log("Joined voice channel 24/7");
+
 };
