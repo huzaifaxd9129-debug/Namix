fd52ed35b4b175896f2f2e22e4650de6fd739d59
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 21:58:34 +0500

    messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,19 @@
+module.exports = async (client, message) => {
 
+if (message.author.bot) return;
+if (!message.content.startsWith(client.prefix)) return;
+
+const args = message.content
+.slice(client.prefix.length)
+.trim()
+.split(/ +/);
+
+const cmd = args.shift().toLowerCase();
+
+client.commands.forEach(command => {
+command.run(client, message, args, cmd);
+});
+
+require("../systems/automod")(message);
+
+};
