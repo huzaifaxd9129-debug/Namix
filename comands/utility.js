f35e2b1dc53adf6c32217aa6294f9925fdde2d02
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 22:01:29 +0500

    utility.js

--- comands/utility.js
+++ comands/utility.js
@@ -1,1 +1,30 @@
+const {
+EmbedBuilder
+} = require("discord.js");
 
+module.exports = {
+name: "utility",
+
+async run(client, message, args, cmd) {
+
+if (cmd === "embed") {
+
+const text = args.join(" ").split("|");
+
+const embed = new EmbedBuilder()
+.setColor("Blue")
+.setTitle(text[0])
+.setDescription(text[1]);
+
+message.channel.send({
+embeds: [embed]
+});
+}
+
+if (cmd === "ping") {
+
+message.reply("Pong!");
+}
+
+}
+};
