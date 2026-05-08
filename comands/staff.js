33c79c1d7432bb2438b4d87d19785aedfa04ddf9
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-08 22:04:12 +0500

    staff.js

--- comands/staff.js
+++ comands/staff.js
@@ -1,1 +1,48 @@
+module.exports = {
+name: "staff",
 
+async run(client, message, args, cmd) {
+
+if (cmd === "apply") {
+
+message.channel.send(`
+STAFF APPLICATION
+
+1. Name?
+2. Age?
+3. Experience?
+4. Why should we pick you?
+5. Activity hours?
+`);
+}
+
+if (cmd === "accept") {
+
+const logs =
+message.guild.channels.cache.get(
+"1500169350307647488"
+);
+
+logs.send(
+`${message.author.tag} accepted application`
+);
+
+message.channel.send("Application accepted");
+}
+
+if (cmd === "reject") {
+
+const logs =
+message.guild.channels.cache.get(
+"1500169350307647488"
+);
+
+logs.send(
+`${message.author.tag} rejected application`
+);
+
+message.channel.send("Application rejected");
+}
+
+}
+};
